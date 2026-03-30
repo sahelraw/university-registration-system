8e0fb531ff3555f22453fe0c9e2b6de0ff935aa8
sahelraw <sahel.alrawabdeh@gmail.com>
2026-03-30 15:56:04 +0300

    R

--- src/test/java/com/uni/demo/DemoApplicationTests.java
+++ src/test/java/com/uni/demo/DemoApplicationTests.java
@@ -4,14 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
-    "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-    "spring.jpa.hibernate.ddl-auto=create-drop"
-})
 class DemoApplicationTests {
 
 	@Test
